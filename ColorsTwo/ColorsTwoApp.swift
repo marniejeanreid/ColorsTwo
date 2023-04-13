8679e61b760729bf44b3271403dfc8dbf9d17b06
Marnie Reid <marnie@kodewithklossy.com>
2023-04-13 09:01:51 +0100

    Initial Commit

---
+++ ColorsTwo/ColorsTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsTwoApp.swift
+//  ColorsTwo
+//
+//  Created by scholar on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorsTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
